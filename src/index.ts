8562d41a432edc3d82e2f3884ed999f326c85a6d
edman-cota <edmancota2017@gmail.com>
2022-11-06 22:55:05 -0600

    chore(tooltip): reorganize files

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import Tooltip from './packages/components/tooltip/Tooltip'
+import Tooltip from './packages/components/tooltip/src/Tooltip'
 import Avatar from './packages/components/avatar/src/Avatar'
 
 export { Tooltip, Avatar }
