3b34914d965305a10b5d188e219d10176210810a
serity-ui <serityuidesign@gmail.com>
2022-11-19 23:45:57 -0600

    fix: type 'undefinied' is not assignable to type Element

--- src/packages/components/tooltip/src/Portal.tsx
+++ src/packages/components/tooltip/src/Portal.tsx
@@ -1,9 +1,7 @@
 import ReactDOM from 'react-dom'
 
 function Portal(props: any) {
-  if(typeof window !== 'undefined') {
     return ReactDOM.createPortal(props.children, document.body)
-  }
 }
 
 export default Portal
