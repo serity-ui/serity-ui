ff32846b2f834be9bf765faaa1341d6c8f6b57e1
edman-cota <edmancota2017@gmail.com>
2022-11-19 23:29:22 -0600

    feat(select): final code for Select component phase 1

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import Tooltip from './packages/components/tooltip/src/Tooltip'
 import Avatar from './packages/components/avatar/src/Avatar'
-import Select from './packages/components/select/src/Select'
+import Select, {SelectItem} from './packages/components/select/src/Select'
 
-export { Tooltip, Avatar, Select }
+export { Tooltip, Avatar, Select, SelectItem }
--- src/packages/components/select/src/select.tsx
+++ src/packages/components/select/src/select.tsx
@@ -1,39 +1,44 @@
-import React, {
-  cloneElement,
-  createContext,
-  useContext,
-  useState,
-} from "react";
-import "./styles.css"
+import React, { createContext, useContext, useState } from 'react'
+import './styles.css'
 
-interface ListProps {
+interface Props {
   children: any
+  value?: string
 }
 
-// const SelectContext = createContext()
+interface TriggerProps {
+  value?: string
+  placeholder: string
+}
 
-export default function Select({children}) {
+const ThemeContext = createContext('')
 
+export default function Select({ children }: Props) {
+  const [toggle, setToggle] = useState(false)
+  const [theme, setTheme] = useState<string>('')
   return (
-    <div className={`nice-select`} >
-      {children}
-    </div>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div className={`nice-select ${toggle ? 'active' : ''}`} onClick={() => setToggle(!toggle)}>
+        {children}
+      </div>
+    </ThemeContext.Provider>
   )
 }
 
-Select.Trigger = function ({ children }) {
-  return(
-    {children}
-  )
-};
+Select.Trigger = function SelectTrigger({ value, placeholder }: TriggerProps) {
+  const { theme } = useContext(ThemeContext)
+  let valor = value
+  if (theme !== '') {
+    valor = theme
+  }
+  return <input type='text' placeholder={placeholder} value={valor} readOnly />
+}
 
+Select.Content = function SelectContent({ children }: Props) {
+  return <ul className='list'>{children}</ul>
+}
 
-// Select.Content = function({ children}) {
-//   const { isCollapsed} = useContext(SelectContext)
-//   return (
-//       <ul className='list'>
-//         {children}
-//       </ul>
-//   )
-// }
-
+export const SelectItem = ({ children, value }: Props) => {
+  const { setTheme } = useContext(ThemeContext)
+  return <li onClick={() => setTheme(value)}>{children}</li>
+}
